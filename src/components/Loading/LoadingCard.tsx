e73482ee010f09334527b9af8c13dec01941d7e7
ousiruon <ou.dehmani@gmail.com>
2025-01-22 11:56:48 -0500

    Layout fixes

--- src/components/Loading/LoadingCard.tsx
+++ src/components/Loading/LoadingCard.tsx
@@ -1,47 +1,59 @@
 const LoadingCard = () => {
   return (
     <>
-      <div className="gameCard flex flex-wrap w-1/4 max-xl:w-1/3 max-md:w-1/2 rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor grow gap-2">
-        <div className="w-full">
-          <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+      <div className="flex flex-wrap w-1/3 max-xl:w-1/2 max-md:w-full rounded-lg p-3 items-center justify-center">
+        <div className="flex flex-wrap w-full rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor">
+          <div className="w-full">
+            <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+          </div>
+          <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
+          <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
         </div>
-        <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
-        <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
       </div>
-      <div className="gameCard flex flex-wrap w-1/4 max-xl:w-1/3 max-md:w-1/2 rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor grow gap-2">
-        <div className="w-full">
-          <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+      <div className="flex flex-wrap w-1/3 max-xl:w-1/2 max-md:w-full rounded-lg p-3 items-center justify-center">
+        <div className="flex flex-wrap w-full rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor">
+          <div className="w-full">
+            <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+          </div>
+          <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
+          <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
         </div>
-        <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
-        <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
       </div>
-      <div className="gameCard flex flex-wrap w-1/4 max-xl:w-1/3 max-md:w-1/2 rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor grow gap-2">
-        <div className="w-full">
-          <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+      <div className="flex flex-wrap w-1/3 max-xl:w-1/2 max-md:w-full rounded-lg p-3 items-center justify-center">
+        <div className="flex flex-wrap w-full rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor">
+          <div className="w-full">
+            <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+          </div>
+          <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
+          <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
         </div>
-        <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
-        <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
       </div>
-      <div className="gameCard flex flex-wrap w-1/4 max-xl:w-1/3 max-md:w-1/2 rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor grow gap-2">
-        <div className="w-full">
-          <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+      <div className="flex flex-wrap w-1/3 max-xl:w-1/2 max-md:w-full rounded-lg p-3 items-center justify-center">
+        <div className="flex flex-wrap w-full rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor">
+          <div className="w-full">
+            <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+          </div>
+          <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
+          <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
         </div>
-        <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
-        <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
       </div>
-      <div className="gameCard flex flex-wrap w-1/4 max-xl:w-1/3 max-md:w-1/2 rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor grow gap-2">
-        <div className="w-full">
-          <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+      <div className="flex flex-wrap w-1/3 max-xl:w-1/2 max-md:w-full rounded-lg p-3 items-center justify-center">
+        <div className="flex flex-wrap w-full rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor">
+          <div className="w-full">
+            <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+          </div>
+          <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
+          <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
         </div>
-        <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
-        <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
       </div>
-      <div className="gameCard flex flex-wrap w-1/4 max-xl:w-1/3 max-md:w-1/2 rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor grow gap-2">
-        <div className="w-full">
-          <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+      <div className="flex flex-wrap w-1/3 max-xl:w-1/2 max-md:w-full rounded-lg p-3 items-center justify-center">
+        <div className="flex flex-wrap w-full rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor">
+          <div className="w-full">
+            <div className="w-full pt-[80%] relative bg-lightMainColor rounded"></div>
+          </div>
+          <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
+          <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
         </div>
-        <div className="flex gap-2 w-full items-center justify-between py-2 h-5 bg-lightMainColor rounded"></div>
-        <div className="flex gap-2 w-full items-center justify-between h-5 bg-lightMainColor rounded"></div>
       </div>
     </>
   );
