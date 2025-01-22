a1ad2e0c7ac2d7232786b37a9ccead2ebd2041b7
ousiruon <ou.dehmani@gmail.com>
2025-01-22 13:13:44 -0500

    Error Page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full min-h-[500px] gap-12">
-        <h1 className="text-6xl font-bold">404</h1>
-        <h1 className="text-5xl font-bold">No Results Found</h1>
-        <span className="text-2xl">
-          Try refining your search criteria or explore different options.
-        </span>
+      <div className="min-h-screen flex w-full items-center justify-center">
+        <div className="flex flex-col items-center justify-center w-full gap-12">
+          <h1 className="text-6xl font-bold">404</h1>
+          <h1 className="text-5xl font-bold">No Results Found</h1>
+          <span className="text-2xl">
+            Try refining your search criteria or explore different options.
+          </span>
+          <Link className="text-2xl font-bold" to="/">
+            Go Back
+          </Link>
+        </div>
       </div>
     </>
   );
