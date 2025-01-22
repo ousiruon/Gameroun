4098597140fb80579e01617c93441f393429ce8a
ousiruon <ou.dehmani@gmail.com>
2025-01-22 13:07:57 -0500

    Error Page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,5 +1,15 @@
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-full min-h-[500px] gap-12">
+        <h1 className="text-6xl font-bold">404</h1>
+        <h1 className="text-5xl font-bold">No Results Found</h1>
+        <span className="text-2xl">
+          Try refining your search criteria or explore different options.
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default ErrorPage;
