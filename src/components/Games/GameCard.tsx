230e566dea33371fea7f0fea976571e98a7fc17d
ousiruon <ou.dehmani@gmail.com>
2025-01-22 11:40:10 -0500

    New Update

--- src/components/Games/GameCard.tsx
+++ src/components/Games/GameCard.tsx
@@ -34,7 +34,7 @@ const GameCard = ({ data }: GameCardProps) => {
     <>
       <motion.div
         key={data.id}
-        className="gameCard flex flex-wrap w-1/3 max-xl:w-1/2 max-md:w-1 rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor"
+        className="gameCard flex flex-wrap w-1/3 max-xl:w-1/2 max-md:w-full rounded-lg p-3 bg-lightBgColor dark:bg-darkBgColor"
         onHoverStart={() => {
           setIsActive(true);
         }}
