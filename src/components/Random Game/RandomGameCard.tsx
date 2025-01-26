f28de1e33f58f48adaab3cba5700837e23284c5a
ousiruon <ou.dehmani@gmail.com>
2025-01-26 08:47:29 -0500

    Navigation button fix

--- src/components/Random Game/RandomGameCard.tsx
+++ src/components/Random Game/RandomGameCard.tsx
@@ -24,7 +24,7 @@ const RandomGameCard = ({
         <div className="my-28 flex flex-col w-1/2 rounded max-md:w-[90%] max-lg:w-[75%]">
           <div className="flex w-full justify-between my-7">
             <motion.div
-              className={`bg-lightBgColor text-lightMainColor dark:bg-darkBgColor dark:text-darkMainColor px-4 py-2 rounded-md cursor-pointer relative flex items-center justify-center overflow-hidden ${
+              className={`bg-lightBgColor text-lightMainColor dark:bg-darkBgColor dark:text-darkMainColor px-4 py-2 rounded-md cursor-pointer font-bold relative flex items-center justify-center overflow-hidden ${
                 currentIndex === 0 && "opacity-0"
               } `}
               onHoverStart={() => setPrevButtonAnimation(true)}
